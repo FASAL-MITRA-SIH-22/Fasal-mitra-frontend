91cdb2395dcffde00634fd67e536dad5d01007f3
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-03-22 00:03:52 +0530

    FEATURE: Updated Signup :peacock:

--- frontend/src/components/Auth/Signup.js
+++ frontend/src/components/Auth/Signup.js
@@ -63,132 +63,62 @@ function Signup() {
           onSubmit={handleSubmit(onSubmit)}
           autoComplete="on"
         >
-          <div className="row-span-1  py-2 mx-auto">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-first-name"
-                >
-                  First Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                  id="grid-first-name"
-                  type="text"
-                  placeholder="Jane"
-                />
-                <p className="text-red-500 text-xs italic">
-                  Please fill out this field.
-                </p>
-              </div>
-              <div className="w-full md:w-1/2 px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-last-name"
-                >
-                  Last Name
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-last-name"
-                  type="text"
-                  placeholder="Doe"
-                />
-              </div>
-            </div>
-            {/* <input
-              className={`shadow appearance-none border rounded w-1/2 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+          <div className="row-span-1 mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="firstName"
+            >
+              First Name
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                 errors && errors.firstName ? "border-red-500" : ""
               }`}
               id="firstName"
               type="text"
-              placeholder="Enter your First Name"
+              placeholder="Enter First Name"
               {...register("firstName")}
             />
             {errors && errors.firstName ? (
-              <p className="text-red-500 text-xs italic w-1/2 ml-6 py-2 px-3">
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
                 {errors.firstName.message}
               </p>
             ) : (
               <></>
             )}
+          </div>
+          <div className="row-span-1 items-center  mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="username"
+            >
+              Username
+            </label>
             <input
-              className={`shadow appearance-none border rounded w-1/2 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                errors && errors.lastName ? "border-red-500" : ""
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors && errors.username ? "border-red-500" : ""
               }`}
-              id="lastName"
+              id="username"
               type="text"
-              placeholder="Enter your Last Name"
-              {...register("lastName")}
+              placeholder="Enter your Username"
+              {...register("username")}
             />
-            {errors && errors.lastName ? (
-              <p className="text-red-500 text-xs italic w-1/2 ml-6 py-2 px-3">
-                {errors.lastName.message}
+            {errors && errors.username ? (
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+                {errors.email.username}
               </p>
             ) : (
               <></>
-            )} */}
+            )}
           </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-password"
-                >
-                  Phone Number
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
-                  type="password"
-                  placeholder="******************"
-                />
-                <p className="text-gray-600 text-xs italic">
-                  Make it as long and as crazy as you'd like
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-password"
-                >
-                  Email ID
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
-                  type="password"
-                  placeholder="******************"
-                />
-                <p className="text-gray-600 text-xs italic">
-                  Make it as long and as crazy as you'd like
-                </p>
-              </div>
-            </div>
-          <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label
-                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-                  for="grid-password"
-                >
-                  Password
-                </label>
-                <input
-                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-password"
-                  type="password"
-                  placeholder="******************"
-                />
-                <p className="text-gray-600 text-xs italic">
-                  Make it as long and as crazy as you'd like
-                </p>
-              </div>
-            </div>
-          <div className="row-span-1 items-center py-2  mx-auto">
-            {/* <input
+          <div className="row-span-1 items-center mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="email"
+            >
+              Email
+            </label>
+            <input
               className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
                 errors && errors.email ? "border-red-500" : ""
               }`}
@@ -198,17 +128,71 @@ function Signup() {
               {...register("email")}
             />
             {errors && errors.email ? (
-              <p className="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
                 {errors.email.message}
               </p>
             ) : (
               <></>
-            )} */}
+            )}
           </div>
-          <div className="row-span-1 items-center py-2  mx-auto">
-            {/* <input
+          <div className="row-span-1 items-center  mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="phone"
+            >
+              Phone
+            </label>
+            <input
               className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                errors && errors.email ? "border-red-500" : ""
+                errors && errors.username ? "border-red-500" : ""
+              }`}
+              id="phone"
+              type="text"
+              placeholder="Enter your Phone Number"
+              {...register("phone")}
+            />
+            {errors && errors.phone ? (
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+                {errors.email.phone}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="row-span-1 mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="lastName"
+            >
+              Last Name
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors && errors.lastName ? "border-red-500" : ""
+              }`}
+              id="lastName"
+              type="text"
+              placeholder="Enter Last Name"
+              {...register("lastName")}
+            />
+            {errors && errors.lastName ? (
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+                {errors.lastName.message}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="row-span-1 items-center mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors && errors.password ? "border-red-500" : ""
               }`}
               id="password"
               type="password"
@@ -216,14 +200,62 @@ function Signup() {
               {...register("password")}
             />
             {errors && errors.password ? (
-              <p className="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
                 {errors.password.message}
               </p>
             ) : (
               <></>
-            )}*/}
+            )}
           </div>
-          <div className="row-span-1 items-center py-2 mx-auto">
+          <div className="row-span-1 items-center mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="password1"
+            >
+              Confirm Password
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors && errors.password1 ? "border-red-500" : ""
+              }`}
+              id="password1"
+              type="password"
+              placeholder="Confirm your Password"
+              {...register("password")}
+            />
+            {errors && errors.password1 ? (
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+                {errors.password1.message}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="row-span-1 items-center mx-auto">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold px-7"
+              for="password1"
+            >
+              Type
+            </label>
+            <input
+              className={`shadow appearance-none border rounded w-96 ml-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                errors && errors.type ? "border-red-500" : ""
+              }`}
+              id="type"
+              type="text"
+              placeholder="Enter your type"
+              {...register("password")}
+            />
+            {errors && errors.type ? (
+              <p class="text-red-500 text-xs italic w-96 ml-6 py-2 px-3">
+                {errors.type.message}
+              </p>
+            ) : (
+              <></>
+            )}
+          </div>
+          <div className="row-span-1 items-center mx-auto">
             <input
               className="bg-emerald-500 border-2 border-emerald-500 hover:bg-white hover:text-emerald-500 text-white font-bold py-2 px-10 rounded-full w-25"
               type="submit"
@@ -231,7 +263,7 @@ function Signup() {
             />
           </div>
         </form>
-      </div> 
+      </div>
     </div>
   );
 }
