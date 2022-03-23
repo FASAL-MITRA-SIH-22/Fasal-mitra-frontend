efe954d88029a84956e2aabf0d4e4812593dca92
Rajas-B <user.emailbondalerajas@gmail.com>
2022-03-23 12:45:19 +0530

    Drag and drop added

--- frontend/src/components/DiseaseDetection/DiseaseDetection.js
+++ frontend/src/components/DiseaseDetection/DiseaseDetection.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import default_crop from "./crop.jpg"
 
 const DiseaseDetection = () => {
@@ -7,6 +7,8 @@ const DiseaseDetection = () => {
   const [isPreview, setIsPreview] = useState();
   const [location, setLocation] = useState(null);
   const [isLocation, setIsLocation] = useState(false);
+  const [isImageCorrect, setIsImageCorrect] = useState(true);
+  const drop = useRef(null);
   useEffect(() => {
     if (!imageUploaded) {
       setPreview(undefined)
@@ -42,6 +44,42 @@ const DiseaseDetection = () => {
     }
     setUploadedImage(e.target.files[0])
   }
+  useEffect(() => {
+    drop.current.addEventListener('dragover', handleDragOver);
+    drop.current.addEventListener('drop', handleDrop);
+  
+    return () => {
+      drop.current.removeEventListener('dragover', handleDragOver);
+      drop.current.removeEventListener('drop', handleDrop);
+    };
+  }, []);
+  
+  const handleDragOver = (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    
+  };
+  
+  const handleDrop = (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+    // this is required to convert FileList object to array
+    if (ev.dataTransfer.items) {
+      console.log(ev.dataTransfer.items)
+      if(ev.dataTransfer.items["length"] == 1){
+        var file = ev.dataTransfer.items[0].getAsFile();
+        if(file && file['type'].split('/')[0] != 'image'){
+          return;
+        }
+        var image = URL.createObjectURL(file);
+        setPreview(image);
+        setIsPreview(true);
+        return () => URL.revokeObjectURL(file);
+      }
+
+    }
+  }
   const submitForm = (e) => {
     e.preventDefault();
     if(preview === undefined){
@@ -70,23 +108,23 @@ const DiseaseDetection = () => {
                 <label for="about" class="block text-sm font-medium text-gray-700">
                   Image
                 </label>
-                <div class="mt-1 flex justify-center">
-                  {!isPreview && <img src={default_crop} class="p-1 bg-white border rounded max-w-sm" required></img>}
+                <div class="mt-1 flex justify-center" >
+                  {!isPreview && <img src={default_crop} class="p-1 bg-white border rounded max-w-sm opacity-50" required></img>}
                   {isPreview && <img src={preview} class="p-1 bg-white border rounded max-w-sm" required></img>}
                 </div>
-                {preview === undefined && 
+                {!isPreview && 
                   <label class="block text-sm font-medium text-red-700">
-                    Please upload image
+                    Please upload a photo
                   </label>
                 }
                 
               </div>
 
-              <div>
+              <div >
                 <label class="block text-sm font-medium text-gray-700">
                   Upload photo of diseased sapling
                 </label>
-                <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                <div ref={drop  } class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                   <div class="space-y-1 text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="True">
                       <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -96,7 +134,7 @@ const DiseaseDetection = () => {
                         <span>Upload a file</span>
                         <input id="file-upload" onChange={imageUploadHandler} name="file-upload" type="file" class="sr-only" accept="image/*"></input>
                       </label>
-                      {/* <p class="pl-1">or drag and drop</p> */}
+                      <p class="pl-1">or drag and drop</p>
                     </div>
                     <p class="text-xs text-gray-500">
                       PNG, JPG, GIF up to 10MB
