bae84d1756a1bcfbadbd06cdda8f5cb3efb9e024
Rajas-B <user.emailbondalerajas@gmail.com>
2022-03-23 17:04:14 +0530

    Added some responsiveness

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import Signup from "./Signup";
 import Login from "./Login";
-
+import './Auth.css'
 
 const Transition = ({setLoginActive, set, name})=>(
-  <div className="container grid grid-rows-3 grid-flow-col content-center gap-4 px-5 my-auto ">
+  <div className="container grid grid-rows-3 grid-flow-col content-center gap-4 px-5 my-auto">
     <div className="text-5xl text-white text-center">Proceed to {name} !</div>
     <p className="text-white text-center">To keep connected with us please {name} with your personal details</p>
     <div className="row-span mx-auto">
@@ -17,16 +17,19 @@ const Transition = ({setLoginActive, set, name})=>(
 
 export default function Auth() {
   const [loginActive, setLoginActive] = useState(true);
+
   return (
     <div className="grid grid-cols-6 h-screen ">
-      <div className={`${loginActive? 'bg-emerald-500 col-span-2': 'bg-white col-span-4'} flex`}>
+      <div className={`${loginActive? 'bg-emerald-500 col-span-2 hide': 'bg-white col-span-4'} flex `}>
         {loginActive ? (
          <Transition setLoginActive={setLoginActive} set={false} name={"Signup"}/>
         ) : (
           <Signup />
+          
+          
         )}
       </div>
-      <div className={`${!loginActive? 'bg-emerald-500 col-span-2': 'bg-white col-span-4'} flex`}>
+      <div className={`${!loginActive? 'bg-emerald-500 col-span-2 hide': 'bg-white col-span-4'} flex `}>
       {!loginActive ? (
          <Transition setLoginActive={setLoginActive} set={true} name={"Login"}/>
         ) : (
