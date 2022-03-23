aa5c0c353b27e1c3d9c754ab11aa0cdf29ba8d3f
Anurag Ghosh <anurag.ghosh@somaiya.edu>
2022-03-23 17:12:28 +0530

    aa

--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -2,36 +2,56 @@ import { useState } from "react";
 import Signup from "./Signup";
 import Login from "./Login";
 
-
-const Transition = ({setLoginActive, set, name})=>(
+const Transition = ({ setLoginActive, set, name }) => (
   <div className="container grid grid-rows-3 grid-flow-col content-center gap-4 px-5 my-auto ">
     <div className="text-5xl text-white text-center">Proceed to {name} !</div>
-    <p className="text-white text-center">To keep connected with us please {name} with your personal details</p>
+    <p className="text-white text-center">
+      To keep connected with us please {name} with your details
+    </p>
     <div className="row-span mx-auto">
-      <button className="bg-transparent border-2 border-white hover:bg-white hover:text-emerald-500 text-white font-bold py-2 px-10 rounded-full w-25" onClick={()=>setLoginActive(set)}>
+      <button
+        className="bg-transparent border-2 border-white hover:bg-white hover:text-emerald-500 text-white font-bold py-2 px-10 rounded-full w-25"
+        onClick={() => setLoginActive(set)}
+      >
         {name}
       </button>
     </div>
   </div>
-)
+);
 
 export default function Auth() {
   const [loginActive, setLoginActive] = useState(true);
   return (
     <div className="grid grid-cols-6 h-screen ">
-      <div className={`${loginActive? 'bg-emerald-500 col-span-2': 'bg-white col-span-4'} flex`}>
+      <div
+        className={`${
+          loginActive ? "bg-emerald-500 col-span-2" : "bg-white col-span-4"
+        } flex`}
+      >
         {loginActive ? (
-         <Transition setLoginActive={setLoginActive} set={false} name={"Signup"}/>
+          <Transition
+            setLoginActive={setLoginActive}
+            set={false}
+            name={"Signup"}
+          />
         ) : (
           <Signup />
         )}
       </div>
-      <div className={`${!loginActive? 'bg-emerald-500 col-span-2': 'bg-white col-span-4'} flex`}>
-      {!loginActive ? (
-         <Transition setLoginActive={setLoginActive} set={true} name={"Login"}/>
+      <div
+        className={`${
+          !loginActive ? "bg-emerald-500 col-span-2" : "bg-white col-span-4"
+        } flex`}
+      >
+        {!loginActive ? (
+          <Transition
+            setLoginActive={setLoginActive}
+            set={true}
+            name={"Login"}
+          />
         ) : (
           <Login />
-          )}
+        )}
       </div>
     </div>
   );
